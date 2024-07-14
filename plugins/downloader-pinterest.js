cfa2c47b440a09eb005a16de22577d2b77ad91a4
Ochoa21 <xavy125427@gmail.com>
2024-07-13 21:52:35 -0500

    si

---
+++ plugins/downloader-pinterest.js
@@ -0,0 +1,13 @@
+import Scraper from "@SumiFX/Scraper"
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return m.reply('👑 Ingresa un texto para realizar la búsqueda.')
+try {
+let { dl_url } = await Scraper.pinterest(text)
+await conn.sendFile(m.chat, dl_url, 'thumbnail.jpg', null, m)
+m.react(done)
+} catch {
+}}
+handler.help = ['pinterest <texto>']
+handler.tags = ['internet']
+handler.command = ['pinterest']
+export default handler
